fa0a04f4b1d4ddbf7fd0954fe9449b9178c5faaf
Aditya Singh <55293868+Adityasingh3010@users.noreply.github.com>
2022-08-21 06:29:34 +0530

    Create 4.java

---
+++ Basics/4.java
@@ -0,0 +1,19 @@
+package test; 
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a=50;
+        boolean ans=true;
+        char name='R';
+        float percentage=99.99f;
+        double mix=998.656;
+
+        System.out.println(a);
+        System.out.println(ans);
+        System.out.println(name);
+        System.out.println(percentage);
+        System.out.println(mix);
+    }
+}
