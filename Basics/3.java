2e7403df450742306caed2031243b9a9bdb16eea
Aditya Singh <55293868+Adityasingh3010@users.noreply.github.com>
2022-08-21 06:18:03 +0530

    Create 3.java

---
+++ Basics/3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        System.out.print("Aditya Kumar Singh"); //single line comment 
+        System.out.println("Aditya Kumar Singh"); //Multi line comment
+        /** doumentary comment **/
+    }
+}
