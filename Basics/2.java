21ca6ab8269fbaf7aa08db0216bc3de2aeb42f29
Adityasingh3010 <sonu.aditya101@gmail.com>
2023-05-13 12:51:46 +0530

    Configuration check

--- Basics/2.java
+++ Basics/2.java
@@ -4,6 +4,6 @@ public class Main //class
 {
     public static void main(String[] args) //method
     {
-        System.out.print("Aditya Kumar Singh");
+        System.out.print("Aditya Singh");
     }
 }
